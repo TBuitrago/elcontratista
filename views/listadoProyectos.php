63ab115a33a67be8130b0619477f2746fce1acd3
Tomas Buitrago <tomasbuitrago6@gmail.com>
2025-05-29 16:14:07 -0500

    Se garantiza que siempre se resuelva la ruta correctamente sin importar desde dónde se ejecute el archivo.

--- views/listadoProyectos.php
+++ views/listadoProyectos.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once '../models/Proyecto.php';
+require_once __DIR__ . '/../models/Proyecto.php';
 $proyecto = new Proyecto();
 
 $categoria = $_GET['categoria'] ?? null;
